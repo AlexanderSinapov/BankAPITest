3395c6f84457c81b15f239ec92aabdb72804c945
TheMoni <unturnedproplayer@gmail.com>
2023-11-12 12:31:07 +0200

    Created Database
    Created Database. I recommend to not use it or touch it because its not fully build and some dependencies are missing.

---
+++ commonJava/src/utils/DBUtils.java
@@ -0,0 +1,17 @@
+package utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import org.json.JSONObject;
+
+public class DBUtils {
+
+    public DBUtils() {
+        String pathToDB = "commonJava/src/database.db";
+    }
+
+    public static JSONObject GetData(){
+        return null;
+    }
+}

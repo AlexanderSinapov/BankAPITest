384b562dc4eaac8739aab3ffd281dbf9ec7730ab
TheMoni <unturnedproplayer@gmail.com>
2023-11-12 14:57:43 +0200

    Finished DBUtils.
    We can now use DBUtils to read and insert from the DB.
    
    Methods:
    GetDataByName(fullName)
    GetDataByEmail(email)
    InsertDataInDB(name, password, dob, pin, phoneNumber, email)
    
    Left to do:
    -Add CreateDebitCard()
    -Add RemoveDebitCard()
    -Bug fixing

--- commonJava/src/main/Main.java
+++ commonJava/src/main/Main.java
@@ -1,7 +1,17 @@
 package main;
+import org.json.JSONArray;
+import utils.DBUtils;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Window window = new Window();
+        // Just for testing don't worry
+        //DBUtils.InsertDataInDB("Simeon Petkov Petkov", "test123", new Date(1990,04,9), 1963, "0885414963", "spetkov273@gmail.com");
+//        JSONArray result = DBUtils.GetDataByName("Simeon Petkov Petkov");
+//        System.out.println(result.toString());
+        //Output:
+        //[{"password":"test123","phoneNumber":"0885414963","pin":1963,"dob":"Fri May 09 00:00:00 EEST 3890","name":"Simeon Petkov Petkov","id":1,"debit_cards":"","email":"spetkov273@gmail.com"}]
     }
 }
--- commonJava/src/utils/DBUtils.java
+++ commonJava/src/utils/DBUtils.java
@@ -1,17 +1,140 @@
 package utils;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class DBUtils {
 
+    private static String url = "jdbc:sqlite:commonJava/src/database.db";
+    private static PreparedStatement p = null;
+    private static ResultSet rs = null;
+
     public DBUtils() {
-        String pathToDB = "commonJava/src/database.db";
+        ;
     }
 
-    public static JSONObject GetData(){
+    private static Connection connectDB(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+
+            Connection conn = DriverManager.getConnection(url);
+            return conn;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
+
+    private static ResultSet GetData(String sql){
+        Connection conn = connectDB();
+
+        try {
+            p = conn.prepareStatement(sql);
+            rs = p.executeQuery();
+            return  rs;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static JSONArray GetDataByName(String fullName){
+        String sql = String.format("select * from users where fullName = '%s'", fullName);
+
+        rs = GetData(sql);
+        JSONArray array = new JSONArray();
+        List data;
+
+        try{
+            while (rs.next()){
+                JSONObject item = new JSONObject();  // Initialize the item here
+                item.clear();
+                int id = rs.getInt("id");
+                String name = rs.getString("fullName");
+                String password = rs.getString("password");
+                String dob = rs.getString("DOB");
+                int Pin = rs.getInt("PIN");
+                String phoneNumber = rs.getString("phoneNumber");
+                String email = rs.getString("email");
+                String debit_cards = rs.getString("debit_cards");
+
+                item.put("id", id);
+                item.put("name", name);
+                item.put("password", password);
+                item.put("dob", dob);
+                item.put("pin", Pin);
+                item.put("phoneNumber", phoneNumber);
+                item.put("email", email);
+                item.put("debit_cards", debit_cards);
+                array.put(item);
+            }
+            return array;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    public static JSONArray GetDataByEmail(String inputEmail){
+        String sql = String.format("select * from users where email = %s", inputEmail);
+
+        rs = GetData(sql);
+        JSONArray array = null;
+        JSONObject item;
+        List data;
+
+        try{
+            while (rs.next()){
+                item = null;
+                int id = rs.getInt("id");
+                String name = rs.getString("fullName");
+                String password = rs.getString("password");
+                Date dob = rs.getDate("DOB");
+                int Pin = rs.getInt("PIN");
+                String phoneNumber = rs.getString("phoneNumber");
+                String email = rs.getString("email");
+                String debit_cards = rs.getString("debit_cards");
+
+                item.put("id", id);
+                item.put("name", name);
+                item.put("password", password);
+                item.put("dob", dob);
+                item.put("pin", Pin);
+                item.put("phoneNumber", phoneNumber);
+                item.put("email", email);
+                item.put("debit_cards", debit_cards);
+                array.put(item);
+            }
+            return array;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    public static void InsertDataInDB(String name, String password, java.util.Date dob, int pin, String phoneNumber, String email){
+        Connection conn = connectDB();
+        String sql = String.format("insert into users values(null, '%s', '%s', '%s', %d, '%s', '%s', '')", name, password, dob.toString(), pin, phoneNumber, email);
+
+        try{
+            Statement st = conn.createStatement();
+            st.execute(sql);
+            System.out.println("Inserted Successfully!");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }

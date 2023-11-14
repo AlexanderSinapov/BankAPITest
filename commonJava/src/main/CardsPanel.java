ac3ce330f56119201ae5d534a4958beef4234166
TheMoni <unturnedproplayer@gmail.com>
2023-11-14 13:52:40 +0200

    Backup

---
+++ commonJava/src/main/CardsPanel.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.*;
+import inputs.MouseInputs;
+import utils.DBUtils;
+
+import javax.swing.border.Border;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CardsPanel extends JPanel {
+}
--- commonJava/src/main/Main.java
+++ commonJava/src/main/Main.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Window window = new Window();
-        DBUtils.ExecuteSQL("DELETE FROM users");
+        //DBUtils.ExecuteSQL("DELETE FROM users");
         //DBUtils.InsertUserInDB("Simeon Petkov Petkov", "hihiha123", new java.util.Date(2009 - 1900, 3, 9), "0885414963", "test123@gmail.com");
     }
 }
